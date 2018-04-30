7e8a95c57c18cacc6c395bbadeb6c7dce0cdc52c
Nathan <linkingitup@outlook.com>
2018-04-30 16:23:00 -0600

    Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { reducer as form} from 'redux-form';
+import { reducer as form } from 'redux-form';
 import newsletterReducer from './newsletterReducer';
 
 const rootReducer = combineReducers({
--- src/reducers/newsletterReducer.js
+++ src/reducers/newsletterReducer.js
@@ -1,9 +1,10 @@
 import { FETCH_NEWSLETTER_ARCHIVE } from "../actions/types";
 
 export default function(state = {}, action) {
-    switch(action.type) {
-        case FETCH_NEWSLETTER_ARCHIVE:
-            return { ...state, archive: action.payload }
-        default: return state;
+
+switch(action.type) {
+   case FETCH_NEWSLETTER_ARCHIVE:
+       return { ...state, archive: action.payload }
+           default: return state;
     }
 }
